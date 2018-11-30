2e7bdba08674a0a4fe43efb05cb5cb93075e0669
suga0828 <alexander.sf0828@gmail.com>
2018-11-29 21:18:41 -0500

    Restaurant model, migrate and controller created

---
+++ app/Http/Controllers/RestaurantController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\restaurant;
+use Illuminate\Http\Request;
+
+class RestaurantController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\restaurant  $restaurant
+     * @return \Illuminate\Http\Response
+     */
+    public function show(restaurant $restaurant)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\restaurant  $restaurant
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(restaurant $restaurant)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\restaurant  $restaurant
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, restaurant $restaurant)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\restaurant  $restaurant
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(restaurant $restaurant)
+    {
+        //
+    }
+}
